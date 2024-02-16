3b935c8e6cd994c84a99695488cdd60e3ab55a96
MaximovMaxim <maximovmaxim100500@gmail.com>
2024-02-16 19:36:40 +0300

    init

---
+++ src/main/java/skypro/HomeworkeSpring/HomeworkeSpringApplication.java
@@ -0,0 +1,13 @@
+package skypro.HomeworkeSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkeSpringApplication.class, args);
+	}
+
+}
